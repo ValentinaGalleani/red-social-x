496138e9cdfefd5fcb03dc6eae1429207555b88f
berenisser <berenisser@gmail.com>
2018-01-04 16:40:37 -0300

    actualización jquery

--- js/app.js
+++ js/app.js
@@ -7,3 +7,17 @@ $(document).ready(function(){
 	}, 1000);
 });
 
+console.log(data);
+
+/**
+* Creo todas las variables que voy a utilizar
+* y guardo en cada una de ellas los div vacíos del html donde irá
+* toda la información sacada de la data
+*/
+var musica = $('.musica');
+var cine = $('.cine');
+var diseño = $('.diseño');
+var teatro =$('.teatro');
+var danza = $('.danza');
+var literatura = $('.literatura');
+var allCategories = $('.all-categories');
