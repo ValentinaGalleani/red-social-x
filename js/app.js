a884ea8638101472a221acd7c611fb5a721cf741
berenisser <berenisser@gmail.com>
2018-01-05 00:14:21 -0300

    agregando login.html en un nuevo archivo html

--- js/app.js
+++ js/app.js
@@ -7,13 +7,14 @@ $(document).ready(function(){
 	}, 1000);
 });
 
+/**
 console.log(data);
 
-/**
+
 * Creo todas las variables que voy a utilizar
 * y guardo en cada una de ellas los div vacíos del html donde irá
 * toda la información sacada de la data
-*/
+
 var musica = $('.Música');
 var cine = $('.Cine');
 var diseño = $('.Diseño');
@@ -21,3 +22,7 @@ var teatro =$('.Teatro');
 var danza = $('.Danza');
 var literatura = $('.Literatura');
 var allCategories = $('.all-categories');
+*/
+
+
+
